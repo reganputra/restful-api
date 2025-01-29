18145ad54640686a70b3622a73dff4d19a501894
reganputra <ramadhanregan334@gmail.com>
2025-01-29 12:37:56 +0700

    feat: add insert data routes

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,7 @@
 //import express
 const express = require('express')
+//import validate post
+const { validatePost } = require('../utils/validators');
 
 //init express router
 const router = express.Router();
@@ -9,6 +11,8 @@ const PostController = require('../controllers/PostController');
 
 //define route for posts
 router.get('/posts', PostController.findPosts);
+//define route for create post
+router.post('/posts', validatePost, PostController.createPost);
 
 //export router
 module.exports = router;
